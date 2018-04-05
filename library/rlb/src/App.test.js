162ae5224b8249a6d912a7aecb564679749f6400
Batterbee <gbatterbee@yahoo.com>
2018-04-05 23:53:21 +0100

    removed non-essential apps

--- library/rlb/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-
-describe('<App />', () => {
-  it('should render without throwing an error', function () {
-      expect(shallow(<App />).find('.App')).toHaveLength(1);
-  });
-});
